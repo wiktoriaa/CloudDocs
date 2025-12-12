5d673a7de9076368560f9524794ab7c4f326ebbb
wlewicka <wlewicka@consdata.com>
2025-12-12 15:56:43 +0100

    Add backend project

---
+++ Backend/CloudStoreBackend/src/main/java/com/example/cloudstorebackend/CloudStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cloudstorebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudStoreBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudStoreBackendApplication.class, args);
+    }
+
+}
---
+++ Backend/CloudStoreBackend/src/test/java/com/example/cloudstorebackend/CloudStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cloudstorebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudStoreBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
