203fd1edd1392f24e927193237570db6bbd3cf76
wlewicka <wlewicka@consdata.com>
2025-12-16 16:00:45 +0100

    Add create document function

--- Frontend/src/app/features/uploader/uploader.ts
+++ Frontend/src/app/features/uploader/uploader.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import {getDownloadURL, ref, uploadBytes} from '@angular/fire/storage';
+import {addDoc, collection} from '@angular/fire/firestore';
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-uploader',
@@ -13,6 +15,15 @@ export class Uploader {
       const storageRef = ref(storage, `documents/${uid}/${file.name}`);
       await uploadBytes(storageRef, file);
       return getDownloadURL(storageRef);
+    }
+
+    public createDocument(uid, url, file, firestore) {
+      addDoc(collection(firestore, 'documents'), {
+        uid,
+        name: file.name,
+        url,
+        createdAt: new Date()
+      });
     }
 
 }
