b90398605059edaa8fda5826ab0cbb5083afdab4
wlewicka <wlewicka@consdata.com>
2026-04-03 13:50:29 +0200

    Angular 21

--- Frontend/src/app/app.config.ts
+++ Frontend/src/app/app.config.ts
@@ -1,16 +1,15 @@
 import {provideBrowserGlobalErrorListeners} from '@angular/core';
 import {provideRouter} from '@angular/router';
-import {bootstrapApplication} from '@angular/platform-browser';
+import {ApplicationConfig} from '@angular/core';
 import {initializeApp, provideFirebaseApp} from '@angular/fire/app';
 import {getAuth, provideAuth} from '@angular/fire/auth';
 import {getFirestore, provideFirestore} from '@angular/fire/firestore';
 import {getStorage, provideStorage} from '@angular/fire/storage';
 
 import {routes} from './app.routes';
-import {App} from './app';
 import {projectConfig} from '../environments/environment.prod';
 
-bootstrapApplication(App, {
+export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
     provideBrowserGlobalErrorListeners(),
@@ -19,4 +18,4 @@ bootstrapApplication(App, {
     provideFirestore(() => getFirestore()),
     provideStorage(() => getStorage()),
   ]
-}).catch(console.error)
+};
--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -6,14 +6,13 @@ import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/mater
 import {MatToolbar} from '@angular/material/toolbar';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {Footer} from './page-template/footer/footer';
-import {CommonModule} from '@angular/common';
 import {filter} from 'rxjs';
 import {AuthService} from './services/auth.service';
 import {MatSnackBar} from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatIcon, MatListItem, MatNavList, MatSidenav, MatSidenavContainer, MatSidenavContent, MatToolbar, MatButton, RouterLink, MatIconButton, Footer, CommonModule],
+  imports: [RouterOutlet, MatIcon, MatListItem, MatNavList, MatSidenav, MatSidenavContainer, MatSidenavContent, MatToolbar, MatButton, RouterLink, MatIconButton, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
